98861cefc81ffe9fc50096bf64da7538ce71772c
Anav Chand <anavkki@gmail.com>
2025-01-13 19:01:10 +1200

    feat: create static home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+    import "@fontsource/geist-sans";
+    import "@fontsource/alice";
+
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main class="w-1/2 mx-auto space-y-5 py-20 text-lg">
+    {@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    const data = [
+        {
+            title: "Socials",
+            links: [
+                 {
+                    label: "GitHub",
+                    href: "href"
+                }
+                ,{
+                    label: "LinkedIn",
+                    href: "href"
+                },
+                {
+                    label: "DockerHub",
+                    href: "href"
+                }
+            ]
+        },
+        {
+            title: "University",
+            links: [
+                {
+                    label: "CS140 Worksheets",
+                    href: "href"
+                },
+                  {
+                    label: "CS112 Worksheets",
+                    href: "href"
+                },
+                { 
+                    label: "CS111 Worksheets",
+                    href: "href"
+                }
+            ]
+        }
+    ]
+</script>
+
+<svelte:head>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta name="description" content="Anav Chand | Links">
+    <title>Anav Chand | Links</title>
+</svelte:head>
+
+<img class="rounded-full h-[200px] w-[200px] mx-auto" src="images/anav.jpg" alt="Anav Chand">
+
+<h1 class="text-center text-5xl">Anav Chand</h1>
+
+<p class="text-center">Hey, I am an aspiring DevOps engineer, self-taught web developer, first-year software engineering student, and award-winning peer mentor @ <a class="underline" href="https://usp.ac.fj">USP</a>.
+</p>
+
+{#each data as {title, links}}
+    <h3 class="text-4xl">{title}</h3>
+    <div class="border border-zinc-200 rounded-lg flex flex-col divide-y divide-zinc-200">
+        {#each links as {label, href}}
+                <a class="p-5" {href}>{label}</a>
+        {/each}
+    </div>
+{/each}
