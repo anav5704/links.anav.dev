5557d986986377214c449a0f851724bee748a70b
Anav Chand <anavkki@gmail.com>
2025-01-17 12:57:09 +1200

    feat: add admin layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,11 @@
     import "@fontsource/geist-sans";
     import "@fontsource/alice";
 
-	import '../app.css';
+    import "../app.css";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
-<main class="w-1/2 mx-auto space-y-5 py-20 text-lg">
+<main class="w-11/12 md:w-3/4 lg:w-1/2 mx-auto space-y-5 text-base md:text-lg">
     {@render children()}
 </main>
