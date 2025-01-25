fb9b8779d7b08fc747de17b029570aea7fb7d54a
Anav Chand <anavkki@gmail.com>
2025-01-25 15:20:58 +1200

    feat: add ssr search

---
+++ src/components/Search.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { getStores } from "$app/stores";
+    import { goto } from "$app/navigation";
+    const { page } = getStores();
+
+    let query = $state($page.url.searchParams.get("search"));
+
+    const handleSearch = () => {
+        goto("?search=" + query, {
+            keepFocus: true,
+            noScroll: true,
+        });
+    };
+</script>
+
+<input
+    placeholder="Search..."
+    oninput={handleSearch}
+    bind:value={query}
+    type="search"
+    class="grow"
+/>
