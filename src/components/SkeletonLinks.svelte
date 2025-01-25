0da14e91dea7325698d3fdff59c10617d34da02b
Anav Chand <anavkki@gmail.com>
2025-01-25 16:09:27 +1200

    style: add skelton links

---
+++ src/components/SkeletonLinks.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+</script>
+
+<div class="flex flex-col gap-5">
+    {#each skeletons as Skeleton}
+        <div class="p-2 bg-zinc-100 rounded-lg animate-pulse text-transparent">
+            <p>Title</p>
+            <p>Subtitle</p>
+        </div>
+    {/each}
+</div>
