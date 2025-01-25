91a99231f737877e199c60f4f5eab734094d3285
Anav Chand <anavkki@gmail.com>
2025-01-25 15:20:18 +1200

    feat: add links crud and search

---
+++ src/routes/(admin)/dash/links/+page.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+    import EditLink from "@/components/forms/EditLink.svelte";
+    import AddLink from "@/components/forms/AddLink.svelte";
+    import Search from "@/components/Search.svelte";
+    import Modal from "@/components/Modal.svelte";
+
+    import { dndzone } from "svelte-dnd-action";
+    import type { Link } from "@prisma/client";
+    import { flip } from "svelte/animate";
+
+    const { data } = $props();
+
+    let links = $state<Link[]>(data.links);
+    let currentLink = $state<Link>();
+    let addLink = $state(false);
+    let editLink = $state(false);
+    const flipDuration = 150;
+
+    const handleConsider = (e: CustomEvent<{ items: Link[] }>) => {
+        links = e.detail.items;
+    };
+
+    const handleFinalize = async (e: CustomEvent<{ items: Link[] }>) => {
+        links = e.detail.items;
+
+        const data = new FormData();
+
+        data.append(
+            "links",
+            JSON.stringify(
+                links.map((link, index) => ({
+                    ...link,
+                    index: ++index,
+                }))
+            )
+        );
+
+        await fetch("?/reorderLink", {
+            method: "POST",
+            body: data,
+        });
+    };
+
+    $effect(() => {
+        links = data.links;
+    });
+</script>
+
+<h1>Manage Links</h1>
+
+<Modal header="Add Link" bind:open={addLink}>
+    <AddLink handleClose={() => (addLink = false)} />
+</Modal>
+
+<Modal header="Edit Link" bind:open={editLink}>
+    <EditLink link={currentLink} handleClose={() => (editLink = false)} />
+</Modal>
+
+<div class="flex gap-5">
+    <Search />
+    <button onclick={() => (addLink = true)}>Create Link</button>
+</div>
+
+<section
+    use:dndzone={{
+        items: links,
+        dropTargetStyle: {},
+        flipDurationMs: flipDuration,
+    }}
+    onconsider={handleConsider}
+    onfinalize={handleFinalize}
+    class="space-y-5"
+>
+    {#each links as link (link.id)}
+        <button
+            animate:flip={{ duration: flipDuration }}
+            class="w-full text-left"
+            onclick={() => {
+                currentLink = link;
+                editLink = true;
+            }}
+        >
+            <p>{link.title}</p>
+            <p class="faded">{link.url}</p>
+        </button>
+    {/each}
+</section>
