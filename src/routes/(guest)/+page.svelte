5dec6ac4638904fe8ce8857a167d356f83da2bdf
Anav Chand <anavkki@gmail.com>
2025-01-25 15:23:20 +1200

    chore: move home page to guest layout

---
+++ src/routes/(guest)/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import data from "../../data/links.json";
+    // import { data } from $props()
+</script>
+
+<svelte:head>
+    <meta name="description" content="Anav Chand | Links" />
+    <title>Anav Chand | Links</title>
+</svelte:head>
+
+<img
+    class="rounded-full h-[150px] w-[150px] md:w-[200px] md:h-[200px] aspect-square mx-auto"
+    src="images/anav.jpg"
+    alt="Anav Chand"
+/>
+
+<h1 class="text-center">Hi, I'm Anav Chand</h1>
+
+<p class="text-center">
+    I'm an aspiring DevOps engineer, self-taught web developer, first-year
+    software engineering student, and award-winning peer mentor @
+    <a class="underline" href="https://usp.ac.fj"> USP </a>.
+</p>
+
+{#each data as { title, href }}
+    <a class="block px-4 py-2 border border-zinc-200 rounded-lg" {href}>
+        <p>{title}</p>
+        <p class="faded">{href}</p>
+    </a>
+{/each}
