7c3c0fa94431e5d8a36890f9da27d444525c2499
Anav Chand <anavkki@gmail.com>
2025-01-25 15:20:39 +1200

    feat: add addlink form

---
+++ src/components/forms/AddLink.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    const { handleClose } = $props();
+</script>
+
+<form action="?/createLink" method="POST">
+    <label>
+        Title
+        <input type="text" name="title" />
+    </label>
+
+    <label>
+        Subtitle
+        <input type="text" name="subtitle" />
+    </label>
+
+    <label>
+        Slug
+        <input type="text" name="slug" />
+    </label>
+
+    <label>
+        URL
+        <input type="text" name="url" />
+    </label>
+
+    <di class="grid grid-cols-2 gap-5">
+        <button class="col-span-1" onclick={handleClose}>Close</button>
+        <button class="col-span-1" type="submit">Create</button>
+    </di>
+</form>
