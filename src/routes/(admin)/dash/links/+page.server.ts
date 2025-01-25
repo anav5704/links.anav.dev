07394a8964fba76ce602acd9f885055d99fb9963
Anav Chand <anavkki@gmail.com>
2025-01-25 15:20:07 +1200

    feat(server): add links crud and search

---
+++ src/routes/(admin)/dash/links/+page.server.ts
@@ -0,0 +1,138 @@
+import { redirect, type Actions } from "@sveltejs/kit"
+import type { Link } from "@prisma/client"
+import { db } from "@/prisma"
+
+export const load = async ({ parent, url }) => {
+    await parent()
+
+    const search = url.searchParams.get("search")
+
+    const links = await db.link.findMany({
+        where: search ? {
+            AND: search.split(" ").map((subSearch) => ({
+                OR: [
+                    {
+                        title: {
+                            contains: subSearch,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        subtitle: {
+                            contains: subSearch,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        slug: {
+                            contains: subSearch,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        url: {
+                            contains: subSearch,
+                            mode: "insensitive"
+                        }
+                    },
+                ],
+            }))
+        } : {},
+        orderBy: {
+            index: "asc"
+        }
+    })
+
+    return { links }
+}
+
+export const actions: Actions = {
+    createLink: async ({ request, locals }) => {
+        if (!locals.user) {
+            redirect(303, "/login")
+        }
+
+        const data = await request.formData()
+
+        const title = data.get("title") as string
+        const subtitle = data.get("subtitle") as string
+        const slug = data.get("slug") as string
+        const url = data.get("url") as string
+
+        await db.link.create({
+            data: {
+                title,
+                subtitle,
+                slug,
+                url
+            }
+        })
+    },
+
+    updateLink: async ({ request, locals }) => {
+        if (!locals.user) {
+            redirect(303, "/login")
+        }
+
+        const data = await request.formData()
+
+        const id = data.get("id") as string
+        const title = data.get("title") as string
+        const subtitle = data.get("subtitle") as string
+        const slug = data.get("slug") as string
+        const url = data.get("url") as string
+
+        await db.link.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                subtitle,
+                slug,
+                url
+            }
+        })
+    },
+
+    reorderLink: async ({ request, locals }) => {
+        if (!locals.user) {
+            redirect(303, "/login")
+        }
+
+        const data = await request.formData()
+
+        const links = JSON.parse(data.get("links") as string)
+
+        await db.$transaction([
+            ...links.map((link: Link) =>
+                db.link.update({
+                    where: {
+                        id: link.id
+
+                    },
+                    data: {
+                        index: link.index
+
+                    }
+                })
+            )])
+    },
+
+    deleteLink: async ({ request, locals }) => {
+        if (!locals.user) {
+            redirect(303, "/login")
+        }
+
+        const data = await request.formData()
+
+        const id = data.get("id") as string
+
+        await db.link.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+}
