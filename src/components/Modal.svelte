84a304041e37be99ab2118859f6441f943124b43
Anav Chand <anavkki@gmail.com>
2025-01-25 15:20:49 +1200

    feat: add modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let { open = $bindable(), header, children } = $props();
+</script>
+
+{#if open}
+    <div
+        class="fixed flex items-center -top-5 left-0 w-screen h-screen bg-black/10"
+    >
+        <div class="mx-auto bg-white p-5 rounded-lg w-11/12 md:w-3/4 lg:w-1/2">
+            <h2 class="mb-5">{header}</h2>
+            {@render children()}
+        </div>
+    </div>
+{/if}
