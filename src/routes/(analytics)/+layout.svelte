80732fb5cca8a47965f7e0ba35e3028cdedc4b61
Anav Chand <anavkki@gmail.com>
2025-02-03 19:10:59 +1200

    chore: remove custom analytics

--- src/routes/(analytics)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    let { children } = $props();
-</script>
-
-<main
-    class="h-screen flex flex-col items-center justify-center gap-5 text-center"
->
-    {@render children()}
-</main>
