46f67d4c2f4c3d9ccc598782e8f47700bcda9136
Anav Chand <anavkki@gmail.com>
2025-02-04 20:55:19 +1200

    style: center modal

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -6,9 +6,13 @@
     <div
         class="z-10 fixed flex items-center -top-5 left-0 w-screen h-screen bg-black/10 backdrop-blur-lg"
     >
-        <div class="mx-auto bg-white p-5 rounded-lg w-11/12 md:w-3/4 lg:w-1/2">
-            <h2 class="mb-5">{header}</h2>
-            {@render children()}
+        <div class="max-w-7xl w-full mx-auto">
+            <div
+                class="mx-auto bg-white p-5 rounded-lg w-11/12 md:w-3/4 lg:w-1/2"
+            >
+                <h2 class="mb-5">{header}</h2>
+                {@render children()}
+            </div>
         </div>
     </div>
 {/if}
